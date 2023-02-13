0c4b45fd9b75aadfb7fe15e865db9964959df6be
Priyesh2612200 <priyesh2612200@gmail.com>
2023-02-13 13:00:01 +0530

    validation work update

--- W8-formJS.js
+++ W8-formJS.js
@@ -10,7 +10,7 @@ $(document).ready(function () {
     });
     $('.txtDate').val('');
     $('.txtDate').attr("placeholder", "MM/DD/YYYY");
-    
+
 
 
     //signature
@@ -30,120 +30,192 @@ $(document).ready(function () {
 
     signatureBox.addEventListener("mousedown", (event) => {
         debugger
-    isDrawing = true;
-    context.clearRect(0, 0, signatureBox.width, signatureBox.height);
-    x = event.clientX - signatureBox.offsetLeft;
-    y = event.clientY - signatureBox.offsetTop;
+        isDrawing = true;
+        context.clearRect(0, 0, signatureBox.width, signatureBox.height);
+        x = event.clientX - signatureBox.offsetLeft;
+        y = event.clientY - signatureBox.offsetTop;
     });
 
     signatureBox.addEventListener("mouseup", () => {
-    isDrawing = false;
+        isDrawing = false;
     });
 
     signatureBox.addEventListener("mousemove", (event) => {
-    if (!isDrawing) {
-        return;
-    }
-    context.beginPath();
-    context.moveTo(x, y);
-    x = event.clientX - signatureBox.offsetLeft;
-    y = event.clientY - signatureBox.offsetTop;
-    context.lineTo(x, y);
-    context.stroke();
+        if (!isDrawing) {
+            return;
+        }
+        context.beginPath();
+        context.moveTo(x, y);
+        x = event.clientX - signatureBox.offsetLeft;
+        y = event.clientY - signatureBox.offsetTop;
+        context.lineTo(x, y);
+        context.stroke();
     });
 
     const clearButton = document.getElementById("clear-button");
     clearButton.addEventListener("click", () => {
-    context.clearRect(0, 0, signatureBox.width, signatureBox.height);
-    context.fillText("Sign Here", signatureBox.width / 2, signatureBox.height / 2);
+        context.clearRect(0, 0, signatureBox.width, signatureBox.height);
+        context.fillText("Sign Here", signatureBox.width / 2, signatureBox.height / 2);
     });
 
 
 
-    $("#submit").click(function(){
-        if(!myFunction()){
+    $("#submit").click(function () {
+        if (!myFunction()) {
             swal("successful");
         }
-        else{
+        else {
             swal("Please Enter all details");
         }
     })
-    function myFunction() { 
-        var valid=false
-        
-            valid= namecheck()
-            if(valid==true)
-                return valid
-            
-                valid= primenamecheck()
-                if(valid==true)
-                    return valid
+    function myFunction() {
+        var valid = false
 
-                    valid= capacitycheck()
-                    if(valid==true)
-                        return valid
-    
+        valid = namecheck()
+        if (valid == true)
+            return valid
+
+
+        valid = ssncheck()
+        if (valid == true)
+            return valid
+
+        valid = ttncheck()
+        if (valid == true)
+            return valid
+
+        valid = ReferenceNumbercheck()
+        if (valid == true)
+            return valid
+
+        valid = primenamecheck()
+        if (valid == true)
+            return valid
+
+        valid = capacitycheck()
+        if (valid == true)
+            return valid
+
+
+
 
     }
-    function namecheck(){
-        var valid=true
+    function namecheck() {
+        var valid = true
         // alert("namecheck")
-        if($("#name").val()==""){
-        $("#namefails").html("Pelase Enter Proper Name")
-        valid=true
+        if ($("#name").val() == "") {
+            $("#namefails").html("Pelase Enter Proper Name")
+            valid = true
         }
-        var string=/^[a-z A-Z]+$/
-        if($("#name").val().match(string)){
-        $("#namefails").html("")
-        valid=false
+        var string = /^[a-z A-Z]+$/
+        if ($("#name").val().match(string)) {
+            $("#namefails").html("")
+            valid = false
         }
-        else{
+        else {
             $("#namefails").html("Pelase Enter Proper Name")
         }
         // alert(valid)
         return valid
     }
 
-    function primenamecheck(){
-        var valid=true
-        // alert("namecheck")
-        if($("#printname").val()==""){
-        $("#printnamevalid").html("Pelase Enter valid Name")
-        valid=true
+
+    function ssncheck() {
+        var valid = true
+       
+        if ($("#SSN").val() == "") {
+            $("#ssnvalid").html("Pelase Enter valid Number")
+            valid = true
         }
-        var string=/^[a-z A-Z]+$/
-        if($("#printname").val().match(string)){
-        $("#printnamevalid").html("")
-        valid=false
+        var number = /^[0-9]+$/
+        if ($("#SSN").val().match(number)) {
+            $("#ssnvalid").html("")
+            valid = false
         }
-        else{
+        else {
+            $("#ssnvalid").html("Pelase Enter Number")
+        }
+        
+        return valid
+    }
+
+    function ttncheck() {
+        var valid = true
+       
+        if ($("#TTN").val() == "") {
+            $("#ttnvalid").html("Pelase Enter valid Number")
+            valid = true
+        }
+        var number = /^[0-9]+$/
+        if ($("#TTN").val().match(number)) {
+            $("#ttnvalid").html("")
+            valid = false
+        }
+        else {
+            $("#ttnvalid").html("Pelase Enter Number")
+        }
+       
+        return valid
+    }
+
+    function ReferenceNumbercheck() {
+        var valid = true
+        
+        if ($("#ReferenceNumber").val() == "") {
+            $("#ReferenceNumbervalid").html("Pelase Enter valid Number")
+            valid = true
+        }
+        var number = /^[0-9]+$/
+        if ($("#ReferenceNumber").val().match(number)) {
+            $("#ReferenceNumbervalid").html("")
+            valid = false
+        }
+        else {
+            $("#ReferenceNumbervalid").html("Pelase Enter Number")
+        }
+       
+        return valid
+    }
+
+
+
+    function primenamecheck() {
+        var valid = true
+        
+        if ($("#printname").val() == "") {
+            $("#printnamevalid").html("Pelase Enter valid Name")
+            valid = true
+        }
+        var string = /^[a-z A-Z]+$/
+        if ($("#printname").val().match(string)) {
+            $("#printnamevalid").html("")
+            valid = false
+        }
+        else {
             $("#printnamevalid").html("Pelase Enter Name")
         }
-        // alert(valid)
+       
         return valid
     }
 
-    function capacitycheck(){
-        var valid=true
-        // alert("namecheck")
-        if($("#capacity").val()==""){
-        $("#capacityvalid").html("Pelase Enter valid Name")
-        valid=true
+    function capacitycheck() {
+        var valid = true
+        
+        if ($("#capacity").val() == "") {
+            $("#capacityvalid").html("Pelase Enter valid Name")
+            valid = true
         }
-        var string=/^[a-z A-Z]+$/
-        if($("#capacity").val().match(string)){
-        $("#capacityvalid").html("")
-        valid=false
+        var string = /^[a-z A-Z]+$/
+        if ($("#capacity").val().match(string)) {
+            $("#capacityvalid").html("")
+            valid = false
         }
-        else{
+        else {
             $("#capacityvalid").html("Pelase Enter Name")
         }
-        // alert(valid)
+        
         return valid
     }
 
-   
-    $('.onlynumber').keyup(function () { 
-        this.value = this.value.replace(/[^0-9\.]/g,'');
-    });
+    
 })
