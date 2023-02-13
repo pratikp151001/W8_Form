1c5d43d60d2d923591715c492650e8d8f539105a
pratikp151001 <121818515+pratikp151001@users.noreply.github.com>
2023-02-13 09:09:36 +0530

    validation_format

--- W8-formJS.js
+++ W8-formJS.js
@@ -59,6 +59,37 @@ $(document).ready(function () {
 
 
 
-    
-
+    $("#submit").click(function(){
+        if(!myFunction()){
+            swal("successful");
+        }
+        else{
+            swal("Please Enter all details");
+        }
+    })
+    function myFunction() { 
+        var valid=false
+        
+            valid= namecheck()
+            if(valid==true)
+                return valid
+    }
+    function namecheck(){
+        var valid=true
+        // alert("namecheck")
+        if($("#name").val()==""){
+        $("#namefails").html("Pelase Enter Proper Name")
+        valid=true
+        }
+        var string=/^[a-zA-Z]+$/
+        if($("#name").val().match(string)){
+        $("#namefails").html("")
+        valid=false
+        }
+        else{
+            $("#namefails").html("Pelase Enter Proper Name")
+        }
+        // alert(valid)
+        return valid
+    }
 })
